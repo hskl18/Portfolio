f032169ccfb876d3204693b93f8e4f6df37c98e6
hanbintan2003 <thb182003@gmail.com>
2023-07-07 04:03:54 -0700

    add more stuf in to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,20 +7,24 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto '>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto
       flex flex-row items-start gap-5`}>
+
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[rgb(119,212,252)]' />
+          <div className='w-1 sm:h-80 h-40 blue-gradient' />
         </div>
+        
         <div>
             <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I am <span className='text-[#915eff]'>
+              Hi, I am <span className='text-[rgb(119,212,252)]'>
                 Hanbin
               </span>
             </h1>
+
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                I develop 3D visuals, user <br className='sm:block hidden'/>iterfaces and web applicationts.
+                I develop 3D visuals, user <br className='sm:block hidden'/>interfaces and web applicationts.
             </p>
           </div>
+          
       </div>
 
       <ComputersCanvas />
