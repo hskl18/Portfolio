f3b10b0754e3c09d8eb97d96ff5af8cd39f336aa
hanbintan2003 <thb182003@gmail.com>
2023-07-08 03:07:05 -0700

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+//for the cards
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      
+    </>
   )
 }
 
