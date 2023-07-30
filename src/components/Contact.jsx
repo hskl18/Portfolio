2ed37866bbcc3ae8ab15b7f10875f2ad42bd7a93
hanbintan2003 <thb182003@gmail.com>
2023-07-30 02:07:11 -0700

    add the last feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,8 +32,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_lm2l3jk',
+        'template_jr2ef8w',
         {
           from_name: form.name,
           to_name: "Hanbin Tan",
@@ -41,7 +41,7 @@ const Contact = () => {
           to_email: "thb182003@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'Pa9a91RuAuqLTGiFI'
       )
       .then(
         () => {
