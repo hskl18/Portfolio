bf563144e145de55127749bf495dcf3329666d5a
hanbintan2003 <thb182003@gmail.com>
2023-07-31 17:56:26 -0700

    testing bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-import { motion } from 'framer-motion'
-import { styles } from '../styles'
-import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
-import { SectionWrapper } from '../hoc'
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -34,37 +34,29 @@ const ServiceCard = ({ index, title, icon }) => (
     </motion.div>
   </Tilt>
 );
+
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>
-          Intro
-        </p>
-        <h2 className={styles.sectionHeadText}>
-          Overviews
-        </h2>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p variants={fadeIn('','',.1,1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'      
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        Hello and welcome to my portfolio! I am Hanbin Tan, hailing from California, 
-        and this platform serves as a showcase of my knowledge and the projects I've undertaken.
-        My journey has involved learning and applying skills in both web development and machine learning. 
-        The projects featured here, which have been published on GitHub, provide tangible evidence of the 
-        lessons I've learned and the expertise I've developed in these fields.
-        Please feel free to explore my portfolio and gain an understanding of my work.
-      
+        Hello and welcome to my portfolio! I am Hanbin Tan, hailing from California, and this platform serves as a showcase of my knowledge and the projects I've undertaken. My journey has involved learning and applying skills in both web development and machine learning. The projects featured here, which have been published on GitHub, provide tangible evidence of the lessons I've learned and the expertise I've developed in these fields. Please feel free to explore my portfolio and gain an understanding of my work.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((services, index) => (
-          <ServiceCard key = {services.title} index = {index}{...services}/>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About,"about")
+export default SectionWrapper(About, "about");
