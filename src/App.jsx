19ae19093110eb30ae5efa412b41481ceeda6619
hskl18 <thb182003@gmail.com>
2024-12-17 16:43:44 -0800

    remove TicTacToe

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, TicTacToe } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  TicTacToe,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-cover bg-no-repeat bg-center min-h-screen bg-[rgb(21,21,21)]'>
-          <StarsCanvas />
-          <Navbar />
-          <Hero />
-          <About />
-          <Tech />
-          <Works />
-          <Experience />
-          <TicTacToe />
-          <Contact />
+      <div className="relative z-0 bg-cover bg-no-repeat bg-center min-h-screen bg-[rgb(21,21,21)]">
+        <StarsCanvas />
+        <Navbar />
+        <Hero />
+        <About />
+        <Tech />
+        <Works />
+        <Experience />
+        {/* <TicTacToe /> */}
+        <Contact />
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
